e4908dd2ad0f007cee3d6dacd39d2f4797dc1b69
Matt Bowen <bowen9284@gmail.com>
2020-08-13 08:22:51 -0400

    adding pinning to map view

--- screens/ViewMapScreen.js
+++ screens/ViewMapScreen.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
+import { setStatusBarNetworkActivityIndicatorVisible } from 'expo-status-bar';
 
 const ViewMapScreen = (props) => {
+  const [selectedLocation, setSelectedLocation] = useState();
+
   const mapRegion = {
     latitude: 37.78,
     longitude: -122.43,
@@ -10,13 +13,39 @@ const ViewMapScreen = (props) => {
     longitudeDelta: 0.0421,
   };
 
-  return <MapView region={mapRegion} style={styles.mapStyle} />;
+  const selectLocationHandler = (event) => {
+    setSelectedLocation({
+      lat: event.nativeEvent.coordinate.latitude,
+      long: event.nativeEvent.coordinate.longitude,
+    });
+  };
+
+  let markerCoordinates;
+
+  if (selectedLocation) {
+    markerCoordinates = {
+      latitude: selectedLocation.lat,
+      longitude: selectedLocation.long,
+    };
+  }
+
+  return (
+    <MapView
+      region={mapRegion}
+      style={styles.mapStyle}
+      onPress={selectLocationHandler}
+    >
+      {markerCoordinates && (
+        <Marker title="Picked Location" coordinate={markerCoordinates}></Marker>
+      )}
+    </MapView>
+  );
 };
 
 export default ViewMapScreen;
 
 const styles = StyleSheet.create({
-    mapStyle: {
-        flex: 1
-    }
+  mapStyle: {
+    flex: 1,
+  },
 });
